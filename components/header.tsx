66d3aa97c0d1c0d8e50193f1ef113335448d54b8
redmojo <donaldchoi5180@gmail.com>
2026-03-14 15:26:39 +0800

    Update Header attribute names

--- components/header.tsx
+++ components/header.tsx
@@ -51,8 +51,8 @@ export default function Header() {
               <text
                 x='30'
                 y='75'
-                font-family="'Brush Script MT', cursive"
-                font-size='50'
+                fontFamily="'Brush Script MT', cursive"
+                fontSize='50'
                 fill='url(#redGradient)'
               >
                 M
@@ -60,8 +60,8 @@ export default function Header() {
 
               <g transform='translate(80, 55) scale(1)'>
                 <path
-                  fill-rule='evenodd'
-                  clip-rule='evenodd'
+                  fillRule='evenodd'
+                  clipRule='evenodd'
                   d='M1.5 12c0-2.25 3.75-7.5 10.5-7.5S22.5 9.75 22.5 12s-3.75 7.5-10.5 7.5S1.5 14.25 1.5 12zM12 16.75a4.75 4.75 0 1 0 0-9.5 4.75 4.75 0 0 0 0 9.5zM14.7 12a2.7 2.7 0 1 1-5.4 0 2.7 2.7 0 0 1 5.4 0z'
                   fill={filledColor}
                 />
@@ -70,19 +70,19 @@ export default function Header() {
               <text
                 x='105'
                 y='75'
-                font-family='Arial, sans-serif'
-                font-size='30'
-                font-weight='bold'
+                fontFamily='Arial, sans-serif'
+                fontSize='30'
+                fontWeight='bold'
                 fill={filledColor}
-                letter-spacing='2'
+                letterSpacing='2'
               >
                 j
               </text>
 
               <g transform='translate(113, 55) '>
                 <path
-                  fill-rule='evenodd'
-                  clip-rule='evenodd'
+                  fillRule='evenodd'
+                  clipRule='evenodd'
                   d='M1.5 12c0-2.25 3.75-7.5 10.5-7.5S22.5 9.75 22.5 12s-3.75 7.5-10.5 7.5S1.5 14.25 1.5 12zM12 16.75a4.75 4.75 0 1 0 0-9.5 4.75 4.75 0 0 0 0 9.5zM14.7 12a2.7 2.7 0 1 1-5.4 0 2.7 2.7 0 0 1 5.4 0z'
                   fill={filledColor}
                 />
